657001edc57260541b089bf1f2d0ef64b9277507
WesBAn <wesban1@gmail.com>
2019-12-09 21:44:34 +0300

    Here's triangle

--- main.cpp
+++ main.cpp
@@ -119,10 +119,19 @@ int main() {
             0.5f, -0.5f, 0.0f,
             0.0f,  0.5f, 0.0f
     };
-    GLuint VBO;
+
+//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GLuint VBO, VAO;
     glGenBuffers(1, &VBO);
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
 
     // Get Shader
     const std::string vertShaderName = "vert.glsl";
@@ -139,8 +148,7 @@ int main() {
     GLuint shaderProgram = getShaderProgram(vertShader, fragmentShader);
     glUseProgram(shaderProgram);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
+
 
     //Start polling
     while(!glfwWindowShouldClose(window))
@@ -149,6 +157,11 @@ int main() {
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
     }
