9860e425f06680fa02a2519a9a5747c76121c743
WesBAn <wesban1@gmail.com>
2019-12-21 02:28:00 +0300

    Changed shaders added gradient

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 
 #define GLEW_STATIC
 #include <GL/glew.h>
 
 #include <GLFW/glfw3.h>
 
+
 enum {
-    INFO_LOG_BUFFER_SIZE = 512
+    INFO_LOG_BUFFER_SIZE = 512,
+    VBO_ARRAY_SIZE = 1,
+    WINDOW_WIDTH = 800,
+    WINDOW_HEIGHT = 600
 };
 
 
@@ -39,6 +44,13 @@ inline void setShowingSize(GLFWwindow* window, GLint x, GLint y, GLint &width, G
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     glfwMakeContextCurrent(window);
+}
+
+
+inline void setBufferVBO(GLuint &VBO, const GLfloat *vertices, const GLuint sizeof_vertices) {
+    glGenBuffers(VBO_ARRAY_SIZE, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof_vertices, vertices, GL_STATIC_DRAW);
 }
 
 
@@ -94,7 +106,11 @@ int main() {
     setGlfwOptions();
 
     // Creating window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Alexander Linnik", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH,
+                                          WINDOW_HEIGHT,
+                                          "Alexander Linnik",
+                                          nullptr,
+                                          nullptr);
     if (!window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -115,22 +131,34 @@ int main() {
 
     // Vertical input
     GLfloat vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
+            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+            -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+    };
+    GLuint indices[] = {
+            0, 1, 2,
+            1, 2, 3
     };
 
-//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    GLuint VBO, VAO;
-    glGenBuffers(1, &VBO);
+    // VBO
+    GLuint VBO;
+    setBufferVBO(VBO, vertices, sizeof(vertices));
 
+    // VAO
+    GLuint VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) 0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) (GLvoid*)(3* sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    // EBO
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     glBindVertexArray(0);
 
     // Get Shader
@@ -146,7 +174,7 @@ int main() {
 
     // Use shaders
     GLuint shaderProgram = getShaderProgram(vertShader, fragmentShader);
-    glUseProgram(shaderProgram);
+//    glUseProgram(shaderProgram);
 
 
 
@@ -159,8 +187,16 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+
+        // Цвет формы
+        GLfloat timeValue = glfwGetTime();
+        GLfloat greenValue = (sin(timeValue) / 2 ) + 0.5;
+        GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
